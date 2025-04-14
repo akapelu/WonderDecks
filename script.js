5dae2ee3e081a0c7070378665d94aaa079bbcabd
Akapelu <akapelugaming@gmail.com>
2025-04-14 01:14:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyADg8wvQZiTCNDG2-ExoCcB10fP2lEN3I",
-    authDomain: "wonderdecks-6ca4f.firebaseapp.com",
-    projectId: "wonderdecks-6ca4f",
-    storageBucket: "wonderdecks-6ca4f.appspot.com",
-    messagingSenderId: "715734231945",
-    appId: "1:715734231945:web:d74cd383ec031e980ecf58",
-    measurementId: "G-Q9DJVZCY6"
+  apiKey: "AIzaSyDq8_wQvQzITCNdoGZmxcoC8jOfP2lEN3I",
+  authDomain: "wonderdecks-6cadf.firebaseapp.com",
+  projectId: "wonderdecks-6cadf",
+  storageBucket: "wonderdecks-6cadf.firebasestorage.app",
+  messagingSenderId: "715734231945",
+  appId: "1:715734231945:web:d74cd383ec031e980ecf58",
+  measurementId: "G-Q9DJVZYCY6"
 };
 
 // Initialize Firebase
